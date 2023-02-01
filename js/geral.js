d36d5ff0200f978d8dca90048c5a3d95ac1db263
Jônatas Alves <jonatas.soares.alves@gmail.com>
2023-01-31 23:27:48 -0300

    Template Inicial

---
+++ js/geral.js
@@ -0,0 +1,6 @@
+// alert("This alert box was called with the onload event");
+$(document).ready(function(){
+    // $("h1").click(function(){
+    //     alert("The paragraph was clicked.");
+    // });
+});

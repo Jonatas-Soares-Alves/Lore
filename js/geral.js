61c64934885f3b6e9445b8aa98b0cb2106d4f7d0
Jônatas Alves <jonatas.soares.alves@gmail.com>
2023-02-26 20:21:38 -0300

    Pagination Sistem

--- js/geral.js
+++ js/geral.js
@@ -1,6 +1,48 @@
-// alert("This alert box was called with the onload event");
+var oldpage = 1;
+var currentPage = 1;
+var maxPages = 1;
+
+function changePage(){
+    console.log('-------------------');
+    console.log('#pg'+oldpage);
+    console.log('#pg'+currentPage);
+    $('#pg'+oldpage).removeClass('d-block');
+    $('#pg'+oldpage).addClass('d-none');
+    $('#pg'+currentPage).removeClass('d-none');
+    $('#pg'+currentPage).addClass('d-block');
+    $('.page-input').val(currentPage);
+}
+
 $(document).ready(function(){
-    // $("h1").click(function(){
-    //     alert("The paragraph was clicked.");
-    // });
+    maxPages = $('.page').length;
+    console.log(maxPages);
+
+    $('.btn-prev').click(function() {
+        if (currentPage > 1) {
+        oldpage = currentPage;
+        currentPage--;
+        }
+        changePage();
+    });
+
+    $('.btn-next').click(function() {
+        if (currentPage < maxPages) {
+        oldpage = currentPage;
+        currentPage++;
+        }
+        changePage();
+    });
+
+    $('.page-input').change(function() {
+        var newPage = parseInt($(this).val());
+        console.log('newPage: '+newPage);
+        oldpage = currentPage;
+        console.log('oldpage: '+oldpage);
+        if (newPage >= 1 && newPage <= maxPages) {
+        currentPage = newPage;
+        }
+        $(this).val(currentPage);
+        console.log('finalpage: '+currentPage);
+        changePage();
+    });
 });
